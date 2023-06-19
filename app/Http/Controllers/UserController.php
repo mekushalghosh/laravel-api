c650e648ff067d9a032b64e2b37be10331da1e33
mekushalghosh <kushal.ghosh@dubswork.com>
2023-06-19 18:46:14 +0530

    Modified post request
    * Added an edge case for empty fields
    
    * Added an edge case for if email is already exists

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,30 @@ class UserController extends Controller
         return $users;
     }
     public function createUser(Request $data){
+
+        //Edge case if anyfield is empty
+        if(!$data-> filled('password') || !$data-> filled('name') || !$data-> filled('email') ){
+            return response()-> json(['message' => 'Some fields are empty, please check again'],200);
+        }
+
+        //Edge case for checking if email already exists or not
+        function checkUniqueEmail(string $email){
+            $emailExists = User::where('email', $email)->exists();
+            return $emailExists;
+        }
+        
+        //If email exists then it will send response back
+        if(checkUniqueEmail($data['email'])){
+            return response()-> json(['message' => 'Email already taken'],200);
+        }
+
         $validatedData = $data -> validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
         ]);
 
+        
         $user = User::create($validatedData);
         return response()-> json(['message' => 'Successfully created user', 'user' => $user],201);
     }
